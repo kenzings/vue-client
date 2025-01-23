9553b486925ab8c25b1f4942b6fcffec5b5e3b0c
Sekai <baonguyen212002@gmail.com>
2025-01-23 11:25:40 +0700

    update post

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        <h1>{{ post.title }}</h1>
-        <p>{{ post.content }}</p>
+    <div class="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-lg">
+        <h1 class="text-3xl font-bold text-gray-800 mb-4">{{ post.title }}</h1>
+        <img :src="post.image" alt="Post Image" class="w-full h-64 object-cover rounded-lg mb-4" />
+        <p class="text-gray-600 leading-relaxed text-lg">
+            {{ post.content }}
+        </p>
     </div>
 </template>
 
@@ -10,8 +13,8 @@ export default {
     props: {
         post: {
             type: Object,
-            required: true
-        }
+            required: true,
+        },
     },
-}
+};
 </script>
